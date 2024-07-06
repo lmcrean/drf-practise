baed6dfb59781f988aaff39ceb8b418f68522395
Laurie C <133490867+lmcrean@users.noreply.github.com>
2024-07-06 13:11:21 +0100

    Initial commit

---
+++ myproject/myapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class HelloWorld(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, world!"}, status=status.HTTP_200_OK)
