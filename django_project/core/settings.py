d186816a9839e2c5a691eb5ec90507fe3cbe0e03
Laurie C <133490867+lmcrean@users.noreply.github.com>
2024-07-07 20:27:52 +0100

    chore: Refactor project structure to move environment files and base files to the base directory

--- django_project/core/settings.py
+++ django_project/core/settings.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
