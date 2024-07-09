bedf8c4074f6b504d1d0a255fc8552e573f2451d
Laurie C <133490867+lmcrean@users.noreply.github.com>
2024-07-09 16:38:16 +0100

    Refactor project structure to update module paths in ASGI and WSGI files

--- django_project/core/wsgi.py
+++ django_project/core/wsgi.py
@@ -6,11 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
+# Path: django_project/core/wsgi.py
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.core.settings')
 
 application = get_wsgi_application()
